cf55f5987d5682abf7e828a5c04e0ce42de034e6
Helfstein <Helfstein-Note>
2018-09-02 14:25:06 -0300

    contract migration success

--- src/contracts/ArticleContract.sol
+++ src/contracts/ArticleContract.sol
@@ -4,11 +4,14 @@ import "../installed_contracts/oraclize-api/contracts/usingOraclize.sol";
 import "../node_modules/openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "./StringUtils.sol";
 
 contract ArticleContract is usingOraclize, Ownable, ERC721Token {
 //==========================================================================
 
     using SafeMath for uint256;
+    using StringUtils for *;
+
     //It will represents a single scientific article
     struct Article {
         string title; //represents a title
@@ -22,7 +25,7 @@ contract ArticleContract is usingOraclize, Ownable, ERC721Token {
 
     mapping(uint => mapping(uint => Article)) data;
     
-    event CallbackReceived(string result);
+    event ArticleResponse(string result);
 
     //----------------------------------------------------------------------------
     constructor(address _oarAddress, string _name, string _symbol) ERC721Token(_name, _symbol) public payable {
@@ -35,7 +38,7 @@ contract ArticleContract is usingOraclize, Ownable, ERC721Token {
     function __callback(bytes32 id, string result, bytes proof) public {
         require(msg.sender == oraclize_cbAddress(), "This address is not a valid coinbase");
 
-        emit CallbackReceived(result);        
+        emit ArticleResponse(result);        
     }
     //----------------------------------------------------------------------------
     // Fallback function
@@ -43,9 +46,13 @@ contract ArticleContract is usingOraclize, Ownable, ERC721Token {
         revert("reverted");
     }
     //----------------------------------------------------------------------------
-    function publishArticle() public payable {
+    function publishArticle(string issn, string author) public payable {
         // oraclize_query("URL", datasource, _article.issn);
-        // oraclize_query("URL", datasource);                
+        var a = "{'issn': '";
+        var b = "', 'author': '";
+        var c = "'}";
+        var payload = strConcat(a, issn, b, author, c);
+        oraclize_query("URL", "json(https://localhost:5001/api/article).accepted", payload);            
     }
 //==========================================================================
 }
