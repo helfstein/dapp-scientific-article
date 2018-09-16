943288ced99d2aeb955704e95add91b9df47d72f
Helfstein <Helfstein-Note>
2018-09-16 03:17:05 -0300

    changing to backend project only

--- src/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#----------------------------------------------------------------------------->
-# This script exists so that we can save all output in the specified log file >
-#----------------------------------------------------------------------------->
-
-# deploy contracts
-truffle migrate --compile-all --reset --network development | tee -a $LOG_FILE
